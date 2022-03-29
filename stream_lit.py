4becdacfa748549141337b8e68659620a18e90c2
FMrtnz <fab.mrtnz22@gmail.com>
2022-03-29 14:04:41 +0100

    Remove useless code

--- stream_lit.py
+++ stream_lit.py
@@ -56,33 +56,3 @@ viz_correlation = sns.heatmap(
 							)
 # We replace plt.show() by st.pyplot()
 st.pyplot(viz_correlation.figure, clear_figure=True)
-
-# Set list of colors for each country
-#colors = {regions_list[0]: "red" , regions_list[1]:"orange", regions_list[2]:"green"}
-
-# define a function to create differents plots scatter
-def loop_plot_scatter(x_axis, y_axis_cols):
-    for col in y_axis_cols:
-        fig, ax = plt.subplots()
-        for region in labels_countries:
-            # Select the region for each axe
-            # Use plot function from panda to set | kind is a param to set plot type
-            df_games[df_games['continent'] == region].copy().plot(kind="scatter",\
-            # ax allows us to association to a fig position\
-            ax = ax, \
-            # Setting X and Y axis\
-            x=x_axis, y=col, ylabel=col, figsize= (10, 3), \
-            # Define color for axe with the color list created \
-            color = colors[region])
-        ax.legend(labels_countries)
-        st.pyplot(fig)
-
-def loop_plot_box(x_axis, y_axis_cols):
-    fig = plt.figure(figsize=(10, 15))
-    i = 1
-    for col in y_axis_cols:
-        ax = plt.subplot(2,2,i)
-        df_games.boxplot(by=x_axis, column=col, ax=ax)
-        ax.set( title = f"{col} vs {x_axis}" )
-        i += 1
-    st.pyplot(fig)
